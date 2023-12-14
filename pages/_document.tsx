7f8d0d960ffaa42f2474aaa52a68716376a066d3
John Njuki <johnnjuki.dev@gmail.com>
2023-12-14 14:31:07 +0300

    rewrite About section

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
+    title: "John's Portfolio",
+    description: "I'm a full-stack developer based in Nairobi, Kenya. I specialize in building (and occasionally designing) exceptional websites, applications, and everything in between.",
+    image: 'images/johnnjuki.jpg', // TODO: Is this working?
   }
 
   return (
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@johnnjuki_" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
