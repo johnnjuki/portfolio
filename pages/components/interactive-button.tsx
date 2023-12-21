3ca8403df629f3d42c3a0197da41b6af06497194
John Njuki <johnnjuki.dev@gmail.com>
2023-12-21 13:35:37 +0300

    add the rest of the projects

---
+++ pages/components/interactive-button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const InteractiveButton = () => {
+  return (
+    <button className="mb-3 h-6 px-2 rounded-xl border border-slate-200 text-slate-900 text-sm hover:cursor-default " type="button">interactive</button>
+  )
+}
