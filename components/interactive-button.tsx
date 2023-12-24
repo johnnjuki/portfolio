7a38875498a1870d98dc69b7b74e791a591ed90a
John Njuki <johnnjuki.dev@gmail.com>
2023-12-24 21:34:32 +0300

    animate the interactive button

--- components/interactive-button.tsx
+++ components/interactive-button.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export const InteractiveButton = () => {
   return (
-    <button className="mb-3 h-6 px-2 rounded-xl border border-slate-200 text-slate-900 text-sm hover:cursor-default " type="button">interactive</button>
-  )
-}
+    // <button className="mb-3 h-6 px-2 rounded-xl border border-slate-200 text-slate-900 text-sm hover:cursor-default "
+
+    <button
+      className="mb-3 h-6 px-2 rounded-xl fancy-border-gradient hover:cursor-default text-sm border-none relative "
+      type="button"
+    >
+      interactive
+    </button>
+  );
+};
