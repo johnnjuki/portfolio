16cec5c8d5dce9f29999974819b46fb4769cd91c
John Njuki <johnnjuki.dev@gmail.com>
2023-12-24 22:43:33 +0300

    update metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   const meta = {
     title: "John's Portfolio",
-    description: "I'm a full-stack developer and I specialize in building exceptional websites, applications, and everything in between.",
+    description: "I'm a full-stack developer and I specialize in building web apps.",
     image: "https://drive.google.com/file/d/1nGVto5BGxE8fE6byHhv0UNGbQtd1o5ym/view?usp=sharing",
   }
 
